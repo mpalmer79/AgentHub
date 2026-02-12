46aa00422aee28e926fcf452cde350f859247403
mpalmer79 <mpalm2025@gmail.com>
2026-02-12 15:26:33 -0500

    Update test_main.py

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,5 +1,12 @@
 """Tests for main application."""
 import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
 
 
 def test_app_starts(client):
